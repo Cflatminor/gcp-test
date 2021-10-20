7e46ff75259245d2048d6925c4945c2757b5196b
Roman Bogdanov <cflatminor92@gmail.com>
2021-10-20 19:25:53 +0300

    init

---
+++ server.js
@@ -0,0 +1,15 @@
+'use strict';
+
+require('dotenv').config();
+
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 8080;
+
+app.get("/" ,(req, res) => {
+    res.send("cc");
+});
+
+app.listen(port, () => {
+    console.log("ready");
+});
